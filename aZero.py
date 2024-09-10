e74eadbcfbb4dbd2ba9c235dd45b1cea0f718ba8
Stephen Hutt <39675459+stephenhutt@users.noreply.github.com>
2024-09-09 19:33:17 -0600

    Initial commit

---
+++ aZero.py
@@ -0,0 +1,61 @@
+#Student Name: <Your Name>
+#Student ID: <Your ID>
+
+import pandas as pd
+import numpy as np
+import scipy
+import A0_Utils as A0
+
+## Question 1 - Basics
+
+def add(a, b):
+    # See Question 1a
+    ## TODO: Define Function
+    ## TODO: comment out "raiseNotDefined()" when you start working on this function
+    A0.raiseNotDefined()
+
+def calcMyGrade(AssignmentScores, MidtermScores, PracticumScores, ICAScores, Weights):
+    # See Question 1b
+    ## TODO: Define Function
+    ## TODO: comment out "raiseNotDefined()" when you start working on this function
+    A0.raiseNotDefined()
+
+
+## Question 2 - Classes
+
+class node:
+    # See Question 2a
+    def __init__(self) -> object:
+        A0.raiseNotDefined()
+
+class queue:
+    # See Question 2b
+    def __init__(self) -> object:
+        A0.raiseNotDefined()
+
+
+## Question 3 - Libraries
+
+def generateMatrix(numRows, numcolumns, minVal, maxVal):
+    # See Question 3ai
+    ## TODO: Define Function
+    ## TODO: comment out "raiseNotDefined()" when you start working on this function
+    A0.raiseNotDefined()
+
+def multiplyMat(m1, m2):
+    # See Question 3a_ii
+    ## TODO: Define Function
+    ## TODO: comment out "raiseNotDefined()" when you start working on this function
+    A0.raiseNotDefined()
+
+def statsTuple(a, b):
+    # See Question 3b
+    ## TODO: Define Function
+    ## TODO: comment out "raiseNotDefined()" when you start working on this function
+    A0.raiseNotDefined()
+
+def pandas_func(fileName):
+    # See Question 3c
+    ## TODO: Define Function
+    ## TODO: comment out "raiseNotDefined()" when you start working on this function
+    A0.raiseNotDefined()
